6b5d1cd539aa240d4db3ae93c453b30bcb75bd44
Utshav Saha <sahaushno@gmail.com>
2025-03-17 12:57:32 +0600

    17 march

--- Linked List/L1.cpp
+++ Linked List/L1.cpp
@@ -63,7 +63,7 @@ void print(Node* head){
     cout << endl; // Add a newline for better output formatting
 }
 
-Node* deletion_first(Node* head){
+Node* delete_head(Node* head){
     if(head == NULL) return head;
 
     Node* temp = head;
@@ -73,12 +73,142 @@ Node* deletion_first(Node* head){
 
 }
 
+Node* delete_tail(Node* head)
+{
+    if(head == NULL || head->next == NULL) return NULL;
+
+    Node *temp = head;
+    while(temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+    return head;
+}
+
+Node* remove_k(Node* head, int k)
+{
+    if (head == NULL) return head;
+    if(k == 1){
+        Node * temp = head;
+        head = head->next;
+
+        free(temp);
+        return head;
+
+    }
+
+    int count = 0;
+    Node* temp = head;
+    Node * prev = NULL;
+
+    while (temp)
+    {
+        count ++;
+        if(count == k)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+
+        prev = temp;
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+Node * insert_head(Node* head, int val)
+{
+    Node * temp = new Node(val , head);
+    return temp;
+}
+
+Node * insert_tail(Node*head, int val)
+{
+    if(head == NULL) return new Node(val);
+    Node * temp = head;
+
+    while(temp->next){
+        temp = temp->next;
+    }
+    Node * newnode = new Node(val);
+    temp-> next = newnode;
+    return head;
+}
+
+Node * insert_k (Node * head , int val, int k)
+{
+    if(head == NULL)
+    {
+        if(k==1) return new Node(val);
+        else return NULL;
+    }
+
+    if(k==1)
+    {
+        Node * temp = new Node(val , head);
+        return temp;
+    }
+
+    int count = 0;
+    Node * temp = head;
+
+    while(temp)
+    {
+        count ++;
+        if(count == k-1){
+            Node * x = new Node(val);
+            x->next = temp->next;
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+
+}
+
+Node * insert_before_val (Node * head , int el, int val)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+
+    if(head->data == val)
+    {
+        Node * temp = new Node(el , head);
+        return temp;
+    }
+
+    
+    Node * temp = head;
+
+    while(temp->next)
+    {
+        
+        if(temp->next->data == val){
+            Node * x = new Node(el);
+            x->next = temp->next;
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+
+}
 int main()
 {
     vector <int> v = {12, 2, 31, 4, 5};
     Node* head = convertArr2LL(v);
     print(head);
-    head = deletion_first(head);
+    head = insert_before_val(head, 11,31);
     print(head);
 
     return 0; // Add return statement
