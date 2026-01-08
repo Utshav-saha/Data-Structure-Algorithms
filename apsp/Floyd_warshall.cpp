4e1a803a00ec3b2c86e4f3bca1a5315d0430d813
Utshav Saha <sahaushno@gmail.com>
2026-01-08 23:43:34 +0600

    8 Jan

--- apsp/Floyd_warshall.cpp
+++ apsp/Floyd_warshall.cpp
@@ -1,30 +1,58 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Solves the all-pairs shortest path
-// problem using Floyd Warshall algorithm
-void floydWarshall(vector<vector<int>> &dist) {
-    int V = dist.size();
 
-    // Add all vertices one by one to
-    // the set of intermediate vertices.
-    for (int k = 0; k < V; k++) {
+void floydWarshall(vector<vector<long long>> &dist) {
+    long long V = dist.size();
 
-        // Pick all vertices as source one by one
-        for (int i = 0; i < V; i++) {
+    
+    for (long long k = 0; k < V; k++) {
 
-            // Pick all vertices as destination
-            // for the above picked source
-            for (int j = 0; j < V; j++) {
+        for (long long i = 0; i < V; i++) {
 
-                // shortest path from
-                // i to j 
+            
+            for (long long j = 0; j < V; j++) {
+
+                
                 if(dist[i][k] != 1e8 && dist[k][j]!= 1e8)
                 dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
             }
         }
     }
+}
+
+int main(){
+
+    long long n , m , q;
+    cin >> n >> m >> q;
+    vector<vector<long long>> dist(n, vector<long long>(n, 1e8));
+
+    for(long long i=0;i<n;i++){
+        dist[i][i] = 0;
+    }
+
+    for(long long i=0; i<m; i++){
+        long long x , y,w;
+        cin >> x >> y >> w;
+        dist[x-1][y-1] = min(dist[x-1][y-1],w);
+        dist[y-1][x-1] = dist[x-1][y-1];
+
+    }
+
+    floydWarshall(dist);
+
+    for(long long i=0; i<q; i++){
+        long long a, b;
+        cin >> a >> b;
+
+        if(dist[a-1][b-1] == 1e8){
+            cout << -1 << endl;
+        }
+        else{
+            cout << dist[a-1][b-1] << endl;
+        } 
+
+    }
+
 }
 
